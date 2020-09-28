8a9cd9471e38ea6ff96291830f684c248070be55
yuki shimaoka_home <yuki_shimaoka@solver.co.jp>
2020-09-28 12:56:20 +0900

    ・新規登録処理　追加 ・DBデータ読み込み処理　追加 ・一覧表示　追加

--- DataEntry_CS/Program.cs
+++ DataEntry_CS/Program.cs
@@ -18,10 +18,11 @@ namespace DataEntry_CS
 
             // 起動時処理
 
+
             while(true)
             {
                 // メインメニュー表示
-                Console.WriteLine(resource.TEXT);
+                Console.WriteLine(resource.MAINMENU_TEXT);
 
                 // 機能選択（入力）
                 var menuNumber = int.Parse(Console.ReadLine());
@@ -31,11 +32,12 @@ namespace DataEntry_CS
                 {
                     // 登録
                     case 1:
-
+                        AddUser();
                         break;
 
                     // 一覧表示
                     case 2:
+                        DispInfo();
                         break;
 
                     // 未定
@@ -62,27 +64,57 @@ namespace DataEntry_CS
         * 登録処理
         * 
         */
-        public bool AddUser()
+        public static bool AddUser()
         {
             Models.HumanInfo humaninfo = new Models.HumanInfo();
 
-            // 会社名 文章出力
-            Console.WriteLine(resource.TEXT);
-
             // 会社名入力
-            humaninfo.CompanyName = Console.ReadLine();
+            humaninfo.CompanyName = controller.inputStringCommon(resource.ADD_COMPANYNAME_TEXT);
+            // null・から文字列・空白文字列の場合、関数を抜ける
+            if(String.IsNullOrWhiteSpace(humaninfo.CompanyName))
+            {
+                return false;
+            }
 
+            // 部署名入力
+            humaninfo.Department = controller.inputStringCommon(resource.ADD_DEPARTMENT_TEXT);
+            // null・から文字列・空白文字列の場合、関数を抜ける
+            if (String.IsNullOrWhiteSpace(humaninfo.Department))
+            {
+                return false;
+            }
 
+            // 役職名入力
+            humaninfo.Position = controller.inputStringCommon(resource.ADD_POSITION_TEXT);
+            // null・から文字列・空白文字列の場合、関数を抜ける
+            if (String.IsNullOrWhiteSpace(humaninfo.Position))
+            {
+                return false;
+            }
 
+            // 氏名入力
+            humaninfo.Name = controller.inputStringCommon(resource.ADD_NAME_TEXT);
+            // null・から文字列・空白文字列の場合、関数を抜ける
+            if (String.IsNullOrWhiteSpace(humaninfo.Name))
+            {
+                return false;
+            }
 
+            // フリガナ入力
+            humaninfo.ReadingKana = controller.inputStringCommon(resource.ADD_READINGKANA_TEXT);
+            // null・から文字列・空白文字列の場合、関数を抜ける
+            if (String.IsNullOrWhiteSpace(humaninfo.ReadingKana))
+            {
+                return false;
+            }
 
-
-
-
-
-
-
-
+            // 年齢入力
+            humaninfo.Old = controller.inputIntCommon(resource.ADD_OLD_TEXT);
+            // 0歳の場合、関数を抜ける
+            if (humaninfo.Old == 0)
+            {
+                return false;
+            }
 
 
             // DB登録処理
@@ -97,5 +129,34 @@ namespace DataEntry_CS
             return true;
         }
 
+
+        /***
+        * 一覧表示処理
+        * 
+        */
+        public static void DispInfo()
+        {
+            var users = GetUserInfo();
+
+            foreach(var user in users)
+            {
+                Console.WriteLine("{0} {1}\n", user.Id, user.Name);
+            }
+ 
+        }
+
+
+        /***
+        * ユーザー情報読み込み
+        * 
+        */
+        public static IEnumerable<Models.HumanInfo> GetUserInfo()
+        {
+            using (var db = new Models.HumanInfoDbContext())
+            {
+                return db.HumanInfos
+                    .ToList();
+            }
+        }
     }
 }
--- DataEntry_CS/controller.cs
+++ DataEntry_CS/controller.cs
@@ -6,14 +6,37 @@ using System.Threading.Tasks;
 
 namespace DataEntry_CS
 {
-    class controller
+    public class controller
     {
-        // データを追加する
+        /***
+         * データ入力共通処理（文字列）
+        */
+        public static string inputStringCommon(string inputText)
+        {
+            // 文章出力
+            Console.WriteLine(inputText);
+
+            // 入力
+            var res = Console.ReadLine() ?? null;
+
+            return res;
+        }
 
 
 
+        /***
+         * データ入力共通処理（数値）
+        */
+        public static int inputIntCommon(string inputText)
+        {
+            // 文章出力
+            Console.WriteLine(inputText);
 
+            // 入力
+            var res = int.Parse(Console.ReadLine() ?? null);
 
+            return res;
+        }
 
 
     }
